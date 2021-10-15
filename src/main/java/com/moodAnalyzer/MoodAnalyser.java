d047de47f2b66cecf744712511eab77d4511fe27
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-10-15 21:22:52 +0530

    Added : null  mood throws exception

--- src/main/java/com/moodAnalyzer/MoodAnalyser.java
+++ src/main/java/com/moodAnalyzer/MoodAnalyser.java
@@ -7,19 +7,18 @@ public class MoodAnalyser {
 		this.message = message;
 	}
 
-	public String analyseMood() {
+	public String analyseMood() throws MoodAnalyserException {
 		try {
 			if (message.contains("sad"))
 				return "sad";
 			else
 				return "Happy";
-		} catch (NullPointerException exception) {
-			return "Happy";
+		} catch (NullPointerException e) {
+			throw new MoodAnalyserException("Please enter valid message");
 		}
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Mood Analyser");
-
 	}
 }
--- src/test/java/com/moodAnalyzer/MoodAnalyserTest.java
+++ src/test/java/com/moodAnalyzer/MoodAnalyserTest.java
@@ -1,28 +1,52 @@
 package com.moodAnalyzer;
-
 import org.junit.Assert;
 import org.junit.Test;
+public class MoodAnalyserTest
+{
+    @Test
+    public void iAmIn_SadMood_ShouldReturn_Sad() throws MoodAnalyserException
+    {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+        String  mood = moodAnalyser.analyseMood();
+        try
+        {
+            mood = moodAnalyser.analyseMood();
+        } catch (MoodAnalyserException e)
+        {
+            e.printStackTrace();
+        }
+        Assert.assertEquals("sad" , mood);
+    }
+    @Test
+    public void iAmIn_anyMood_ShouldReturn_Happy() throws MoodAnalyserException
+    {
 
-public class MoodAnalyserTest {
-	@Test
-	public void iAmIn_SadMood_ShouldReturn_sad() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("sad", mood);
-	}
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
+        String mood = moodAnalyser.analyseMood();
+        try
+        {
+            mood = moodAnalyser.analyseMood();
+        } catch (MoodAnalyserException e)
+        {
+            e.printStackTrace();
+        }
+        Assert.assertEquals("Happy" , mood);
+    }
 
-	@Test
-	public void iAmIn_anyMood_ShouldReturn_happy() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("Happy", mood);
-	}
+    @Test
+    public void when_Null_Should_Return_Message() throws MoodAnalyserException {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        try
+        {
+            moodAnalyser.analyseMood();
+        } catch (MoodAnalyserException e)
+        {
+            Assert.assertEquals("Please enter valid message", e.getMessage());
+            System.out.println("Enter valid message");
+        }
 
-	@Test
-	public void null_Should_Return_Happy_Mood() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("Happy", mood);
-	}
+
+    }
 
 }
+
