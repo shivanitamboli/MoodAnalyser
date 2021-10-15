85486add7de1ef6fb87c7af210e30a22ad3274d1
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-10-15 21:35:30 +0530

    Added : repeat the test case using constructor

--- src/main/java/com/moodAnalyzer/MoodAnalyser.java
+++ src/main/java/com/moodAnalyzer/MoodAnalyser.java
@@ -7,18 +7,19 @@ public class MoodAnalyser {
 		this.message = message;
 	}
 
-	public String analyseMood() throws MoodAnalyserException {
+	public String analyseMood() {
 		try {
 			if (message.contains("sad"))
 				return "sad";
 			else
 				return "Happy";
-		} catch (NullPointerException e) {
-			throw new MoodAnalyserException("Please enter valid message");
+		} catch (NullPointerException exception) {
+			return "Happy";
 		}
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Mood Analyser");
+
 	}
 }
